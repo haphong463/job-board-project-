3d148a872f43c441d0fe5944e762e16bf6f1cc76
3tlus <102000987+3tlus@users.noreply.github.com>
2024-05-20 05:13:04 +0700

    Update CategoryController.java
    update Nam

--- JobBoardService/src/main/java/com/project4/JobBoardService/Controller/CategoryController.java
+++ JobBoardService/src/main/java/com/project4/JobBoardService/Controller/CategoryController.java
@@ -9,27 +9,52 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@CrossOrigin
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping
-    public List<Category> getAllCategories() {
-        return categoryService.getAllCategories();
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping("/categories")
+    public List<Category> getAllCategory() {
+        return categoryRepository.findAll();
     }
 
-    @GetMapping("/{id}")
-    public Category getCategoryById(@PathVariable Long id) {
-        return categoryService.getCategorybyId(id);
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<Category> getCategorybyId(@PathVariable(value = "id") Long categoryId)
+            throws ResourceNotFoundException {
+        Category employee = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found for this id :: " + categoryId));
+        return ResponseEntity.ok().body(employee);
     }
 
-    @PostMapping
-    public void saveCategory(@RequestBody Category category) {
-        categoryService.savedCategory(category);
+    @PostMapping("/categories")
+    public Category createCategory(@Validated @RequestBody Category category) {
+        return categoryRepository.save(category);
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteCategoryById(@PathVariable Long id) {
-        categoryService.deleteCategorybyId(id);
+    @PutMapping("/categories/{id}")
+    public ResponseEntity<Category> updateEmployee(@PathVariable(value = "id") Long categoryId,
+                                                   @Validated @RequestBody Category categoryDetails) throws ResourceNotFoundException {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found for this id :: " + categoryId));
+        category.setCategoryName(categoryDetails.getCategoryName());
+        final Category updatedCategory = categoryRepository.save(category);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("/categories/{id}")
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long categoryId)
+            throws ResourceNotFoundException {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found for this id :: " + categoryId));
+
+        categoryRepository.delete(category);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 }
+
