84cd968915edd1e781eb9b501be610ae349ded46
3tlus <102000987+3tlus@users.noreply.github.com>
2024-05-22 07:49:56 +0700

    update Nam

---
+++ JobBoardService/src/main/java/com/project4/JobBoardService/Service/ReviewService.java
@@ -0,0 +1,17 @@
+package Project4.JobBoard.Service;
+
+import Project4.JobBoard.DTO.ReviewDTO;
+import Project4.JobBoard.Entity.Review;
+
+import java.util.List;
+
+public interface ReviewService {
+    List<Review> getAllReviews(Long companyId);
+
+    boolean addReview(Long companyId, Review review);
+
+    Review getReview(Long companyId, Long reviewId);
+
+    boolean updateReview(Long companyId, Long reviewId, Review updatedReview);
+
+}
