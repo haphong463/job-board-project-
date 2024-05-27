d3f12c8abb81a70a681a3d261f7cb41efb5a175f
haphong463 <haphong463@gmail.com>
2024-05-28 03:02:20 +0700

    dependency, layouts: add more dependency and layout for portal

--- JobBoardManagement/src/index.js
+++ JobBoardManagement/src/index.js
@@ -1,21 +1,21 @@
 import React, { Suspense } from "react";
 // import ReactDOM from "react-dom";
-import {createRoot} from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import "./assets/scss/style.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Loader from "./layouts/loader/Loader";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <Suspense fallback={<Loader />}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
-  </Suspense>,
+    </BrowserRouter>
+  </Suspense>
 
   // document.getElementById("root")
 );
