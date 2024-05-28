daba54d9b9fbbb7079f1943e8cf247f6c7c4e0fa
luongnhatnam <luongnhatnam1102@gmail.com>
2024-05-28 13:36:26 +0700

    'update Nam'

---
+++ JobBoardService/src/main/java/com/project4/JobBoardService/Entity/FavoriteJob.java
@@ -0,0 +1,27 @@
+package com.project4.JobBoardService.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "favorite_job")
+public class FavoriteJob {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Job job;
+
+    @ManyToOne
+    private User user;
+
+}
