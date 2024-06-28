d9aa4dccf8dc266a72501c94ff3bd9b93591c0aa
haphong463 <haphong463@gmail.com>
2024-06-28 12:43:37 +0700

    refactor: blog service

---
+++ JobBoardService/src/main/java/com/project4/JobBoardService/Controller/TokenRequest.java
@@ -0,0 +1,11 @@
+package com.project4.JobBoardService.Controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenRequest {
+    private String token;
+
+}
