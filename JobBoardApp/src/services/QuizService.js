8cb408e886b8e570bc4d784d2727e055b8b45fc2
3tlus <102000987+3tlus@users.noreply.github.com>
2024-06-30 08:38:42 +0700

    Update QuizService.js

--- JobBoardApp/src/services/QuizService.js
+++ JobBoardApp/src/services/QuizService.js
@@ -3,3 +3,19 @@ import axiosRequest from "../configs/axiosConfig";
 export const getQuizzes = () => {
   return axiosRequest.get('/quizzes');
 };
+
+export const getQuizDetails = (quizId) => {
+  return axiosRequest.get(`/quizzes/${quizId}`);
+};
+export const getQuizQuestions = (quizId) => {
+  return axiosRequest.get(`/quizzes/${quizId}/questions`);
+};
+
+export const submitQuiz = async (submission) => {
+  try {
+    const response = await axiosRequest.post('/quizzes/submit', submission);
+    return response.data; // Assuming server returns an array of results
+  } catch (error) {
+    throw error; // Propagate the error back to the caller
+  }
+};
