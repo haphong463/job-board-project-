9d236ecd520d94929ffa69a7b114208923c66cbe
haphong463 <haphong463@gmail.com>
2024-08-02 00:05:31 +0700

    update hashtag for blog

--- jobboardmobile/lib/service/blog_service.dart
+++ jobboardmobile/lib/service/blog_service.dart
@@ -2,15 +2,22 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:jobboardmobile/constant/endpoint.dart';
 import 'package:jobboardmobile/models/blog_model.dart';
+import 'package:jobboardmobile/models/category_model.dart';
 import 'package:jobboardmobile/models/content_model.dart';
 
 class BlogService {
   final String baseUrl = '${Endpoint.baseUrl}/blogs';
+  final String categoryUrl = '${Endpoint.baseUrl}/blog-category';
 
   Future<BlogModel> searchBlogs(String query, String type,
-      {int page = 0, int size = 10, int visibility = 2}) async {
+      {int page = 0,
+      int size = 10,
+      int visibility = 2,
+      String order = "asc"}) async {
+    final encodedCategory = Uri.encodeComponent(type);
+
     final response = await http.get(Uri.parse(
-        '$baseUrl/search?query=$query&type=$type&page=$page&size=$size&visibility=$visibility'));
+        '$baseUrl/search?query=$query&type=$encodedCategory&page=$page&size=$size&visibility=$visibility&order=$order'));
 
     if (response.statusCode == 200) {
       final jsonResponse = json.decode(response.body);
@@ -20,16 +27,29 @@ class BlogService {
     }
   }
 
-  Future<Content?> getBlogBySlug(String slug) async {
+  Future<ContentModel?> getBlogBySlug(String slug) async {
     try {
       final response = await http.get(Uri.parse('$baseUrl/$slug'));
       if (response.statusCode == 200) {
         final jsonResponse = json.decode(response.body);
-        return Content.fromJson(jsonResponse);
+        return ContentModel.fromJson(jsonResponse);
       }
     } catch (e) {
       return null;
     }
     return null;
+  }
+
+  Future<List<Category>> getBlogCategories() async {
+    try {
+      final response = await http.get(Uri.parse(categoryUrl));
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body) as List;
+        return jsonResponse.map((data) => Category.fromJson(data)).toList();
+      }
+    } catch (e) {
+      throw Exception('Failed to load blog categories');
+    }
+    return [];
   }
 }
